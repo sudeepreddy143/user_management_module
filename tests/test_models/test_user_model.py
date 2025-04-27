9430ddabf772b4f206c8d7af4eebac83bf9a8166
sudeepreddy143 <sudeeppanyam@gmail.com>
2025-04-27 19:05:47 +0530

    updated the test_model as per latest user_model

--- tests/test_models/test_user_model.py
+++ tests/test_models/test_user_model.py
@@ -28,7 +28,7 @@ async def test_user_repr(user: User):
     """
     Tests the __repr__ method for accurate representation of the User object.
     """
-    assert repr(user) == f"<User {user.nickname}, Role: {user.role.name}>", "__repr__ should include nickname and role"
+    assert repr(user) == f"<User(id={user.id}, email={user.email}, nickname={user.nickname}, role={user.role})>", "__repr__ should include nickname and role"
 
 @pytest.mark.asyncio
 async def test_failed_login_attempts_increment(db_session: AsyncSession, user: User):
